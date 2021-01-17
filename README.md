b4d829673acc91f22f63045d06dcad40baaaa796
Amaya <42989748+the-amaya@users.noreply.github.com>
2021-01-17 02:59:27 -0500

    initial commit, this works
    this will grab an image from a url and respond to a discord message

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # secbot
+
+<p align="center">
+	<img alt="GitHub language count" src="https://img.shields.io/github/languages/count/the-amaya/secbot?style=plastic">
+	<img alt="GitHub top language" src="https://img.shields.io/github/languages/top/the-amaya/secbot?style=plastic">
+	<img alt="GitHub code size in bytes" src="https://img.shields.io/github/languages/code-size/the-amaya/secbot?style=plastic">
+	<img alt="GitHub" src="https://img.shields.io/github/license/the-amaya/secbot?style=plastic">
+	<img alt="GitHub contributors" src="https://img.shields.io/github/contributors/the-amaya/secbot?style=plastic">
+	<img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/the-amaya/secbot?style=plastic">
+	<img alt="Scrutinizer code quality (GitHub/Bitbucket)" src="https://img.shields.io/scrutinizer/quality/g/the-amaya/secbot?style=plastic">
+</p>
+
  framegrab reolink cameras in discord
---
+++ main.py
@@ -0,0 +1,42 @@
+import discord
+from urllib.request import urlopen
+from PIL import Image
+import io
+
+cameras = {
+	'driveway': '10.0.50.101',
+	'porch': '10.0.50.102',
+	'backdeck': '10.0.50.100',
+	'mudroom': '10.0.50.104',
+	'racoon': '10.0.50.106',
+}
+
+class MyClient(discord.Client):
+
+	async def on_ready(self):
+		print('Logged on as', self.user)
+
+	async def on_message(self, message):
+		word_list = ['driveway', 'porch', 'backdeck', 'mudroom', 'racoon']
+
+		# don't respond to ourselves
+		if message.author == self.user:
+			return
+
+		messageContent = message.content
+		if len(messageContent) > 0:
+			for word in word_list:
+				if word == messageContent:
+					#img = PImage.open(path + image)
+					img_file = urlopen('http://' + cameras.get(messageContent) + '/cgi-bin/api.cgi?cmd=Snap&channel=0&rs=wuuPhkmUCeI9WG7C&user=admin&password=camera')
+					img = Image.open(img_file)
+					#await message.channel.to_file(img)
+					#await message.channel.send(file=discord.File(fp=img, filename='image.png'))
+					img.save('img.png')
+					await message.channel.send(file=discord.File('img.png'))
+			if messageContent == 'list':
+				await message.reply(cameras.keys(), mention_author=True)
+
+
+client = MyClient()
+client.run('bot_token')

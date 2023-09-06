29bdbfa2c374ec47d3088f23c72f7d154b307cff
Amaya <42989748+the-amaya@users.noreply.github.com>
2023-09-06 00:48:32 -0400

    eco update
    split the eco database functions from the main cog commands. should be easier to use the database elsewhere.

--- cogs/eco.py
+++ cogs/eco.py
@@ -3,9 +3,8 @@ from discord.ext import commands
 import sqlite3
 
 
-class Eco(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+class economyBackend():
+    def __init__(self):
         self.conn = sqlite3.connect('economy.db')
         self.c = self.conn.cursor()
 
@@ -18,53 +17,65 @@ class Eco(commands.Cog):
             PRIMARY KEY (guild_id, user_id))
         """)
 
+    def checkBalance(self, server, user):
+        self.c.execute("SELECT coins FROM coins WHERE guild_id = ? AND user_id = ?", (server, user))
+        balance = self.c.fetchone()
+        return balance[0]
+
+    def addBalance(self, server, user, amount):
+        # add coins to user
+        self.c.execute("""
+                    INSERT INTO coins(guild_id, user_id, coins)
+                    VALUES(?,?,?) 
+                    ON CONFLICT(guild_id, user_id) 
+                    DO UPDATE SET coins = coins + ?
+                """, (server, user, amount, amount))
+        self.conn.commit()  # need to commit after every UPDATE operation
+
+    def subBalance(self, server, user, amount):
+        # subtract coins from author
+        self.c.execute("""
+                    INSERT INTO coins(guild_id, user_id, coins)
+                    VALUES(?,?,?) 
+                    ON CONFLICT(guild_id, user_id) 
+                    DO UPDATE SET coins = coins - ?
+                """, (server, user, -amount, amount))
+        self.conn.commit()  # need to commit after every UPDATE operation
+
+    def transfer(self, server, fromUser, toUser, amount):
+        self.subBalance(server, fromUser, amount)
+        self.addBalance(server, toUser, amount)
+
+
+class Eco(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.db = economyBackend()
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Cog Eco is ready.')
+
     @commands.command(name='balance', help='Check your current balance')
     async def balance(self, ctx, user: discord.Member = None):
         user = user or ctx.author
-        self.c.execute("SELECT coins FROM coins WHERE guild_id = ? AND user_id = ?", (ctx.guild.id, user.id))
-        balance = self.c.fetchone()
+        balance = self.db.checkBalance(ctx.guild.id, user.id)
         if balance is None:
             await ctx.send(f'{user.display_name} has no coins.')
         else:
-            await ctx.send(f'{user.display_name} has {balance[0]} coins.')
+            await ctx.send(f'{user.display_name} has {balance} coins.')
 
     @commands.command(name='transfer', help='transfer coins to another user. transfer <coins> <user>')
     async def transfer(self, ctx, user: discord.Member, amount: int):
-        # subtract coins from author
-        self.c.execute("""
-            INSERT INTO coins(guild_id, user_id, coins)
-            VALUES(?,?,?) 
-            ON CONFLICT(guild_id, user_id) 
-            DO UPDATE SET coins = coins - ?
-        """, (ctx.guild.id, ctx.author.id, -amount, amount))
-        self.conn.commit()  # need to commit after every UPDATE operation
-
-        # add coins to user
-        self.c.execute("""
-            INSERT INTO coins(guild_id, user_id, coins)
-            VALUES(?,?,?) 
-            ON CONFLICT(guild_id, user_id) 
-            DO UPDATE SET coins = coins + ?
-        """, (ctx.guild.id, user.id, amount, amount))
-        self.conn.commit()  # need to commit after every UPDATE operation
+        self.db.subBalance(ctx.guild.id, ctx.author.id, amount)
+        self.db.addBalance(ctx.guild.id, user.id, amount)
         await ctx.send(f'{ctx.author.display_name} has given {user.display_name} {amount} coins.')
 
     @commands.command(name='earn', help='you can earn coins, simply specify the number you want!')
     async def earn(self, ctx, amount: int):
-        # add coins to the author
-        self.c.execute("""
-            INSERT INTO coins(guild_id, user_id, coins)
-            VALUES(?,?,?) 
-            ON CONFLICT(guild_id, user_id) 
-            DO UPDATE SET coins = coins + ?
-        """, (ctx.guild.id, ctx.author.id, amount, amount))
-        self.conn.commit()  # need to commit after every UPDATE operation
-        await ctx.send(f'{ctx.author.display_name} has earned {amount} coins.')
-
-    # commit changes and close connection when the bot exits
-    def cog_unload(self):
-        self.conn.commit()
-        self.conn.close()
+        self.db.addBalance(ctx.guild.id, ctx.author.id, amount)
+        balance = self.db.checkBalance(ctx.guild.id, ctx.author.id)
+        await ctx.send(f'{ctx.author.display_name} has earned {amount} coins. Your new balance is {balance}')
 
 
 async def setup(bot):

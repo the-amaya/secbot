72197688fdede2c919f29aedb029e5b15e854a21
Amaya <42989748+the-amaya@users.noreply.github.com>
2023-09-06 22:50:09 -0400

    Update eco.py

--- cogs/eco.py
+++ cogs/eco.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import sqlite3
 
 
-class economyBackend():
+class economyBackend:
     def __init__(self):
         self.conn = sqlite3.connect('economy.db')
         self.c = self.conn.cursor()
@@ -17,12 +17,12 @@ class economyBackend():
             PRIMARY KEY (guild_id, user_id))
         """)
 
-    def checkBalance(self, server, user):
+    def checkBalance(self, server: int, user: int) -> int:
         self.c.execute("SELECT coins FROM coins WHERE guild_id = ? AND user_id = ?", (server, user))
         balance = self.c.fetchone()
         return balance[0]
 
-    def addBalance(self, server, user, amount):
+    def addBalance(self, server: int, user: int, amount: int) -> bool:
         # add coins to user
         self.c.execute("""
                     INSERT INTO coins(guild_id, user_id, coins)
@@ -31,24 +31,36 @@ class economyBackend():
                     DO UPDATE SET coins = coins + ?
                 """, (server, user, amount, amount))
         self.conn.commit()  # need to commit after every UPDATE operation
+        return True
 
-    def subBalance(self, server, user, amount):
-        # subtract coins from author
-        self.c.execute("""
-                    INSERT INTO coins(guild_id, user_id, coins)
-                    VALUES(?,?,?) 
-                    ON CONFLICT(guild_id, user_id) 
-                    DO UPDATE SET coins = coins - ?
-                """, (server, user, -amount, amount))
-        self.conn.commit()  # need to commit after every UPDATE operation
+    def subBalance(self, server: int, user: int, amount: int) -> bool:
+        # subtract coins from specified user
+        # returns false on insufficent funds
+        # start by checking the balance
+        bal = self.checkBalance(server, user)
+        if amount > bal:
+            # insufficent funds
+            return False
+        else:
+            self.c.execute("""
+                          INSERT INTO coins(guild_id, user_id, coins)
+                          VALUES(?,?,?) 
+                          ON CONFLICT(guild_id, user_id) 
+                          DO UPDATE SET coins = coins - ?
+                          """, (server, user, -amount, amount))
+            self.conn.commit()  # need to commit after every UPDATE operation\
+            return True
 
-    def transfer(self, server, fromUser, toUser, amount):
-        self.subBalance(server, fromUser, amount)
-        self.addBalance(server, toUser, amount)
+    def transfer(self, server: int, fromUser: int, toUser: int, amount: int) -> bool:
+        if self.subBalance(server, fromUser, amount):
+            self.addBalance(server, toUser, amount)
+            return True
+        else:
+            return False
 
 
 class Eco(commands.Cog):
-    "The basic economy functions"
+    """The basic economy functions"""
     def __init__(self, bot):
         self.bot = bot
         self.db = economyBackend()
@@ -68,15 +80,20 @@ class Eco(commands.Cog):
 
     @commands.command(name='transfer', help='transfer coins to another user. transfer <coins> <user>')
     async def transfer(self, ctx, user: discord.Member, amount: int):
-        self.db.subBalance(ctx.guild.id, ctx.author.id, amount)
-        self.db.addBalance(ctx.guild.id, user.id, amount)
-        await ctx.send(f'{ctx.author.display_name} has given {user.display_name} {amount} coins.')
+        if self.db.transfer(ctx.guild.id, ctx.author.id, user.id, amount):
+            await ctx.send(f'{ctx.author.display_name} has given {user.display_name} {amount} coins.')
+        else:
+            await ctx.send(f'{ctx.author.display_name} does not have enough coins to give {amount} to '
+                           f'{user.display_name}. You can check your balance with the balance command')
 
     @commands.command(name='earn', help='you can earn coins, simply specify the number you want!')
     async def earn(self, ctx, amount: int):
-        self.db.addBalance(ctx.guild.id, ctx.author.id, amount)
-        balance = self.db.checkBalance(ctx.guild.id, ctx.author.id)
-        await ctx.send(f'{ctx.author.display_name} has earned {amount} coins. Your new balance is {balance}')
+        if self.db.addBalance(ctx.guild.id, ctx.author.id, amount):
+            balance = self.db.checkBalance(ctx.guild.id, ctx.author.id)
+            await ctx.send(f'{ctx.author.display_name} has earned {amount} coins. Your new balance is {balance}')
+        else:
+            await ctx.send(f'I am sorry, but I was unable to add coins to your balance for some reason, sorry about '
+                           f'that {ctx.author.display_name}, maybe try again or @ the bot owner')
 
 
 async def setup(bot):
